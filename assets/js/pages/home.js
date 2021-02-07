a490d7ee4452a15be123ca905be3bb7f3e4ea49c
Lucas <lucas.antunes355@gmail.com>
2021-02-07 16:54:41 -0300

    Home - Remove "apenas"

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,30 +1,34 @@
-const divProdutos = $('#divProdutos');
-const divVendas = $('#divVendas');
-const divCobrancas = $('#divCobrancas');
-const divClientes = $('#divClientes');
-const divEstoqueWarning = $('#divEstoqueWarning');
+const divProdutos = $("#divProdutos");
+const divVendas = $("#divVendas");
+const divCobrancas = $("#divCobrancas");
+const divClientes = $("#divClientes");
+const divEstoqueWarning = $("#divEstoqueWarning");
 
 $(document).ready(() => {
-    axios.post("Home/getIndexTotalizers").then(({ data }) => {
-        divProdutos.text(data.produtos);
-        divVendas.text(data.vendas);
-        divCobrancas.text(data.cobrancas);
-        divClientes.text(data.clientes);
-    })
+  axios.post("Home/getIndexTotalizers").then(({ data }) => {
+    divProdutos.text(data.produtos);
+    divVendas.text(data.vendas);
+    divCobrancas.text(data.cobrancas);
+    divClientes.text(data.clientes);
+  });
 
-    axios.post("Home/getLowStock").then(({ data }) => {
-        data.forEach(element => {
-            if (element.quantidade <= 10) {
-                divEstoqueWarning.append(`
+  axios.post("Home/getLowStock").then(({ data }) => {
+    data.forEach((element) => {
+      if (element.quantidade <= 10) {
+        divEstoqueWarning.append(`
                 <div class="customer-message align-items-center">
                     <a class="font-weight-bold" href="#">
-                        <div class="text-truncate message-title">O produto '${element.descricao}' possui apenas ${element.quantidade} unidade(s) em estoque!</div>
+                        <div class="text-truncate message-title">O produto '${
+                          element.descricao
+                        }' possui ${
+          element.quantidade
+        } unidade(s) em estoque!</div>
                         <div class="small text-gray-500 message-time font-weight-bold">Última Movimentação: ${new Date(
-                    Date.parse(element.ultima_movimentacao)
-                ).toLocaleString("en-GB")}</div >
+                          Date.parse(element.ultima_movimentacao)
+                        ).toLocaleString("en-GB")}</div >
                     </a >
                 </div > `);
-            }
-        });
-    })
-})
+      }
+    });
+  });
+});
